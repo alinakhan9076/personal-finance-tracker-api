b9ac6de224b2a04934a6e7a61f8c1754f037fd77
alinakhan9076 <alinak.dev04@gmail.com>
2026-09-17 20:47:00 +0530

    Add user authentication

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+const authMiddleware = require("./middleware/authMiddleware");
+
+dotenv.config();
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.get("/api/expenses", authMiddleware,
+     (req, res) => {
+    res.json([]);
+});
+
 const PORT = 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB connected");
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+});
+})
+.catch((error) =>{
+    console.error("MongoDB connection failed:", error.message);
 });
